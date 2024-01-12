1ac809334b0863bbec17b193ddc47fbadba3ac81
Renat <renat.zayika@gmail.com>
2024-01-12 23:12:59 +0100

    improved

--- Test Alta Games/Assets/Scripts/Architecture/States/LoadGameState.cs
+++ Test Alta Games/Assets/Scripts/Architecture/States/LoadGameState.cs
@@ -1,6 +1,8 @@
 using Architecture.Services.Interfaces;
 using Architecture.States.Interfaces;
 using Audio;
+using Data;
+using UnityEngine;
 
 namespace Architecture.States
 {
@@ -11,13 +13,18 @@ namespace Architecture.States
         private readonly ISceneLoader _sceneLoader;
         private readonly IAudioService _audioService;
         private readonly IAssetProvider _assetProvider;
+        private readonly IBaseFactory _baseFactory;
+        private readonly GameSettings _gameSettings;
 
         public LoadGameState(ISceneLoader sceneLoader, IAudioService audioService, 
-            IAssetProvider assetProvider)
+            IAssetProvider assetProvider, IBaseFactory baseFactory, 
+            GameSettings gameSettings)
         {
             _sceneLoader = sceneLoader;
             _audioService = audioService;
             _assetProvider = assetProvider;
+            _baseFactory = baseFactory;
+            _gameSettings = gameSettings;
         }
         
         public void Exit()
@@ -32,8 +39,14 @@ namespace Architecture.States
             _sceneLoader.Load(GameScene, Initialize);
         }
 
-        private void Initialize()
+        private async void Initialize()
         {
+            Transform parent = (await _baseFactory.CreateAddressableWithObject
+                (_gameSettings.BaseParent, Vector3.zero, Quaternion.identity, null)).transform;
+            
+            Camera camera = (await _baseFactory.CreateAddressableWithContainer
+                (_gameSettings.BaseCamera, Vector3.zero, Quaternion.identity, parent)).GetComponent<Camera>();
+            
             _audioService.PlayMusic(MusicType.Game);
         }
     }
